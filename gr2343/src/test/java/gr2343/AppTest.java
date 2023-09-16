4b2c381a498415e5f71656baa84a77c37b701b21
Anna Farestrand <annatfa@stud.ntnu.no>
2023-09-16 13:03:57 +0200

    Wrote minimal pseudocode for testing

--- gr2343/src/test/java/gr2343/AppTest.java
+++ gr2343/src/test/java/gr2343/AppTest.java
@@ -17,3 +17,14 @@ public class AppTest
         assertTrue(true);
     }
 }
+
+/**  test for input i tekstfelt
+ * hvis input inneholder minst én bokstav så er dte ok, for det kan være feks. A3.
+ * ikke ok hvis det kun er tall eller andre tegn
+ * kan f.eks. loope over strenger og bruke isalpha() el.l.
+*/
+
+/** test for input i rangeringsfeltet
+ * hvis input er i mengden [1:10] så er det ok
+ * hvis input er noe annet er det ikke ok
+ */
