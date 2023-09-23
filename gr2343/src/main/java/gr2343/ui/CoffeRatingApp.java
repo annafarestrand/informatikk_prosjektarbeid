473c4f43574eb50b6150f27db3e3f78d47cb809d
sofiefe <sofiefe@stud.ntnu.no>
2023-09-23 15:29:59 +0200

    Set up off app

---
+++ gr2343/src/main/java/gr2343/ui/CoffeRatingApp.java
@@ -0,0 +1,22 @@
+package gr2343.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CoffeRatingApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("CoffeRating.fxml"));
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(CoffeRatingApp.class, args);
+    }
+
+}
