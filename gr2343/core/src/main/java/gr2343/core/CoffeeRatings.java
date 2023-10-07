735016ec4d47ac8e2e323cd3e53f62a357e89766
sofiefe <sofiefe@stud.ntnu.no>
2023-10-07 16:13:32 +0200

    reverted this file, co-authored by Ragni

--- gr2343/core/src/main/java/gr2343/core/CoffeeRatings.java
+++ gr2343/core/src/main/java/gr2343/core/CoffeeRatings.java
@@ -8,26 +8,12 @@ import java.util.List;
 public class CoffeeRatings implements Iterable<CoffeeRatingItem> {
     private List<CoffeeRatingItem> items = new ArrayList<CoffeeRatingItem>();
 
-    public CoffeeRatingItem createCoffeeRatingItem() {
-        return new CoffeeRatingListItem(this);
-    }
-
     public void addCoffeeRatingItem(CoffeeRatingItem item) {
-        CoffeeRatingListItem coffeeRatingListItem = null;
-        if (item instanceof CoffeeRatingListItem) {
-            coffeeRatingListItem = (CoffeeRatingListItem) item;
-        } else {
-            coffeeRatingListItem = new CoffeeRatingListItem(this);
-            coffeeRatingListItem.setDescription(item.getDescription());
-            coffeeRatingListItem.setRating(item.getRating());
-        }
-        // items.add(coffeeRatingListItem); Made a bug, keep it incase of trouble later
-        fireCoffeeRatingChanged();
+        items.add(item);
     }
 
     public void removeCoffeeRatingItem(CoffeeRatingItem item) {
         items.remove(item);
-        fireCoffeeRatingChanged();
     }
 
     @Override
@@ -42,25 +28,4 @@ public class CoffeeRatings implements Iterable<CoffeeRatingItem> {
         }
         return result;
     }
-
-    private Collection<CoffeeRatingListener> cofferatingListeners = new ArrayList<>();
-
-    public void removeCofferatingListener(CoffeeRatingListener listener) {
-        cofferatingListeners.remove(listener);
-    }
-
-    public void addCofferatingListener(CoffeeRatingListener listener) {
-        cofferatingListeners.add(listener);
-    }
-
-    protected void fireCoffeeRatingChanged() {
-        for (CoffeeRatingListener listener : cofferatingListeners) {
-            listener.coffeeRatingChanged(this);
-        }
-    }
-
-    protected void fireCoffeeRatingChanged(CoffeeRatingItem item) {
-        fireCoffeeRatingChanged();
-    }
-
 }
