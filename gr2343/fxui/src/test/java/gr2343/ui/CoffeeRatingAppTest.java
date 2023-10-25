c0e4971639694a78110e7cb168df4c4fd8ec6e10
Anna Farestrand <annatfa@stud.ntnu.no>
2023-10-25 13:26:01 +0200

    Wrote tests for delete and update, wip. Co-Authored-by: Sofie

--- gr2343/fxui/src/test/java/gr2343/ui/CoffeeRatingAppTest.java
+++ gr2343/fxui/src/test/java/gr2343/ui/CoffeeRatingAppTest.java
@@ -3,7 +3,6 @@ package gr2343.ui;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.testfx.framework.junit5.ApplicationTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +12,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 public class CoffeeRatingAppTest extends ApplicationTest {
@@ -73,8 +74,6 @@ public class CoffeeRatingAppTest extends ApplicationTest {
   }
 
   @Test
-
-
   private void checkCoffeeRatingItem(CoffeeRatingItem CoffeeRating, int rating, String text) {
     if (rating != 0) {
       assertEquals(rating, CoffeeRating.getRating());
@@ -103,4 +102,63 @@ public class CoffeeRatingAppTest extends ApplicationTest {
     }
     assertTrue(i == items.length);
   }
+
+  @Test
+  public void testDeleteCoffeRating() {
+    // legger inn en rating som kan slettes
+    String newDescription = "Fra den gode kafeen";
+    String newRating = "5";
+
+    // skriver inn description og rating
+    clickOn("#newDescriptionText").write(newDescription);
+    clickOn("#newRatingText").write(newRating);
+
+    // lagrer
+    clickOn("#newCoffeeRatingButton");
+    CoffeeRatingItem newItem = new CoffeeRatingItem();
+    newItem.setDescription(newDescription);
+    newItem.setRating(Integer.parseInt(newRating));
+
+    // klikke paa det item'et som skal slettes
+    
+    // fra todolist: clickOn(findTodoItemListCellNode(cell -> !cell.getItem().isChecked(), ".check-box", 0));
+
+    // sletter itemet
+    clickOn("#deleteRatingButton");
+
+    // sjekker at riktig items ligger i listen
+    checkCoffeeRatingListItems(item1, item2, newItem);
+    checkCoffeeRatingListViewItems(item1, newItem, item2);
+  }
+
+  @Test
+  public void testUpdateCoffeRating() {
+    // legger inn et item
+    String newDescription1 = "Kontoret mitt";
+    String newRating1 = "5";
+    clickOn("#newDescriptionText").write(newDescription1);
+    clickOn("#newRatingText").write(newRating1);
+    clickOn("#newCoffeeRatingButton");
+
+    CoffeeRatingItem newItem = new CoffeeRatingItem();
+    newItem.setDescription(newDescription1);
+    newItem.setRating(Integer.parseInt(newRating1));
+
+    // angrer paa det vi la inn, vil redigere item'et
+    //clickOn("#ratingsView"); // klikker paa det item'et som ligger der fra foer av...
+
+    // skriver inn ny description og rating og lagrer denne
+    String newDescription2 = "Kontoret ditt";
+    String newRating2 = "3";
+    clickOn("#updateRatingButton");
+    clickOn("#newDescriptionText").write(newDescription2);
+    clickOn("#newRatingText").write(newRating2);
+    newItem.setDescription(newDescription2);
+    newItem.setRating(Integer.parseInt(newRating2));
+
+    checkCoffeeRatingListItems(item1, item2, newItem);
+    checkCoffeeRatingListViewItems(item1, newItem, item2);
+  }
+
+
 }
