ee5db9a6c68e8e2207e44d47803374c08897bc92
sofiefe <sofiefe@stud.ntnu.no>
2023-10-30 10:30:50 +0100

    Added deserializer test

--- gr2343/core/src/test/java/gr2343/json/CoffeeRatingModuleTest.java
+++ gr2343/core/src/test/java/gr2343/json/CoffeeRatingModuleTest.java
@@ -2,9 +2,11 @@ package gr2343.json;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.Iterator;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,6 +17,8 @@ import gr2343.core.CoffeeRatings;
 public class CoffeeRatingModuleTest {
 
     private static ObjectMapper mapper;
+
+    private String json = "{\"items\":[{\"description\":\"Kaffe på Sit Kafe\",\"rating\":5}]}";
 
     @BeforeAll
     public static void setUp() {
@@ -30,12 +34,25 @@ public class CoffeeRatingModuleTest {
         item.setRating(5);
         ratings.addCoffeeRatingItem(item);
         try {
-            assertEquals(
-                    "{\"items\":[{\"description\":\"Kaffe på Sit Kafe\",\"rating\":5}]}",
-                    mapper.writeValueAsString(ratings));
+            assertEquals(json, mapper.writeValueAsString(ratings));
         } catch (JsonProcessingException e) {
             fail();
         }
     }
 
+    @Test
+    public void testDeserializers() {
+        try {
+            CoffeeRatings ratings = mapper.readValue(json, CoffeeRatings.class);
+            Iterator<CoffeeRatingItem> it = ratings.iterator();
+            assertTrue(it.hasNext());
+            CoffeeRatingItem item = it.next();
+            assertEquals("Kaffe på Sit Kafe", item.getDescription());
+            assertEquals(5, item.getRating());
+        } catch (JsonProcessingException e) {
+            fail();
+        } catch (IOException e) {
+            fail();
+        }
+    }
 }
