10412be0d782fa9c4e04a84d766036237e50f5f6
sofiefe <sofiefe@stud.ntnu.no>
2023-11-02 15:18:33 +0100

    Added help function to return rating by name

--- gr2343/core/src/main/java/gr2343/core/CoffeeRatingModel.java
+++ gr2343/core/src/main/java/gr2343/core/CoffeeRatingModel.java
@@ -16,6 +16,15 @@ public class CoffeeRatingModel implements Iterable<CoffeeRatings> {
     ratings.remove(rating);
   }
 
+  public CoffeeRatings getRating(String name) {
+    for (CoffeeRatings rating : ratings) {
+      if (rating.getName().equals(name)) {
+        return rating;
+      }
+    }
+    return null;
+  }
+
   @Override
   public Iterator<CoffeeRatings> iterator() {
     return ratings.iterator();
