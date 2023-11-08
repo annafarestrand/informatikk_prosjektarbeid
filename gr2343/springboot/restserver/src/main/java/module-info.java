0220e8f8b673f5a544bd160f2537c55ea8908e10
Ragni Bonvik <rbonvik@gmail.com>
2023-11-08 15:39:12 +0100

    added moduleinfo for springboot module

--- gr2343/springboot/restserver/src/main/java/module-info.java
+++ gr2343/springboot/restserver/src/main/java/module-info.java
@@ -1,3 +1,14 @@
-module name {
-  
+module com.gr2343.springboot.rest {
+    requires com.fasterxml.jackson.databind;
+
+    requires spring.web;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.context;
+    requires spring.boot.autoconfigure;
+
+    requires com.gr2343.core;
+
+    opens gr2343.springboot.restserver to spring.beans, spring.context, spring.web;
 }
+
