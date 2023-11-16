2f2c27269c2d353a5f93f65a11fbe74f5b118137
sofiefe <sofiefe@stud.ntnu.no>
2023-11-16 12:51:37 +0100

    Updated test, co-authored by Ragni

--- gr2343/core/src/test/java/gr2343/json/CoffeeRatingsPersistenceTest.java
+++ gr2343/core/src/test/java/gr2343/json/CoffeeRatingsPersistenceTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -15,37 +17,117 @@ import gr2343.core.CoffeeRatingModel;
 public class CoffeeRatingsPersistenceTest {
 
     private CoffeeRatingsPersistence persistence;
-    private ObjectMapper mockMapper; // simulerer en ObjectMapper
-    private ObjectWriter mockWriter; // simulerer en ObjectWriter
-    private CoffeeRatingModel mockModel; // simulerer en CoffeeRatingModel
+    private ObjectMapper mockMapper;
+    private ObjectWriter mockWriter;
+    private CoffeeRatingModel mockModel;
 
-    // setter opp mapper, writer og model før hver test
     @BeforeEach
     public void setUp() {
+        // Oppretter mock-objekter
         mockMapper = mock(ObjectMapper.class);
         mockWriter = mock(ObjectWriter.class);
         mockModel = mock(CoffeeRatingModel.class);
 
+        // Simulerer oppførselen til mock-objektene
         when(mockMapper.writerWithDefaultPrettyPrinter()).thenReturn(mockWriter);
 
+        // Oppretter testobjektet
         persistence = new CoffeeRatingsPersistence();
-
-        try {
-            java.lang.reflect.Field field = CoffeeRatingsPersistence.class.getDeclaredField("mapper");
-            field.setAccessible(true);
-            field.set(persistence, mockMapper);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            fail("Failed to set mock ObjectMapper: " + e.getMessage());
-        }
+        // Setter det private feltet mapper ved hjelp av refleksjon
+        setPrivateField(persistence, "mapper", mockMapper);
     }
 
-    // tester om man kan lese en CoffeeRating fra model
     @Test
     public void testReadCoffeeRatings() throws IOException {
+        // Simulerer JSON-data og forventet modell fra mock-objektet
         String sampleJson = "{\"lists\":[{\"name\":\"ratings\",\"items\":[]}]}";
         when(mockMapper.readValue(sampleJson, CoffeeRatingModel.class)).thenReturn(mockModel);
 
+        // Utfører testen
         CoffeeRatingModel result = persistence.readCoffeeRatings(sampleJson);
+        // Sjekker om resultatet er som forventet
         assertEquals(mockModel, result);
+    }
+
+    @Test
+    public void testWriteCoffeeRatingModel() throws IOException {
+        // Oppretter en mock Writer
+        Writer mockWriter = mock(Writer.class);
+        // Utfører skriving til modellen
+        persistence.writeCoffeeRatingModel(mockModel, mockWriter);
+        // Verifiserer at writeValue-metoden på mockWriter ble kalt med riktige argumenter
+        verify(mockMapper.writerWithDefaultPrettyPrinter()).writeValue(mockWriter, mockModel);
+    }
+
+    @Test
+    public void testReadCoffeeRatingModel() throws IOException {
+        // Oppretter en mock Reader
+        Reader mockReader = mock(Reader.class);
+        when(mockMapper.readValue(mockReader, CoffeeRatingModel.class)).thenReturn(mockModel);
+
+        // Utfører lesing fra modellen
+        CoffeeRatingModel result = persistence.readCoffeeRatingModel(mockReader);
+        // Sjekker om resultatet er som forventet
+        assertEquals(mockModel, result);
+    }
+
+    @Test
+    public void testSetSaveFile() {
+        // Definerer en testfil
+        String saveFile = "testFile.json";
+        // Setter filstien ved hjelp av setSaveFile-metoden
+        persistence.setSaveFile(saveFile);
+        // Forventer at filstien er satt korrekt
+        Path saveFilePath = Paths.get(System.getProperty("user.home"), saveFile);
+        assertEquals(saveFilePath, persistence.getSaveFilePath());
+    }
+
+    @Test
+    public void testLoadCoffeeRatingModel() throws IOException {
+        // Setter en testfil for lasting
+        String saveFile = "testFile.json";
+        persistence.setSaveFile(saveFile);
+
+        // Simulerer JSON-innhold og en forventet modell fra mock-objektet
+        String jsonContent = "{\"lists\":[{\"name\":\"ratings\",\"items\":[]}]}";
+        StringReader stringReader = new StringReader(jsonContent);
+        when(mockMapper.readValue(stringReader, CoffeeRatingModel.class)).thenReturn(mockModel);
+
+        // Utfører lasting av modellen
+        CoffeeRatingModel result = persistence.loadCoffeeRatingModel();
+        // Sjekker om resultatet er som forventet
+        assertEquals(mockModel, result);
+    }
+
+    @Test
+    public void testSaveCoffeeRatingModel() throws IOException {
+        // Setter en testfil for lagring
+        String saveFile = "testFile.json";
+        persistence.setSaveFile(saveFile);
+
+
+        // Simulerer oppførselen til mock-objektene
+        when(mockMapper.writerWithDefaultPrettyPrinter()).thenReturn(mockWriter);
+        when(mockWriter.writeValueAsString(mockModel)).thenReturn("mockJson");
+
+        // Utfører lagring av modellen
+        persistence.saveCoffeeRatingModel(mockModel);
+
+        // Verifiserer at writeValue-metoden på mockWriter ble kalt med riktige argumenter
+        verify(mockWriter).writeValue(any(FileWriter.class), eq(mockModel));
+    }
+
+    // Hjelpemetode for å sette private felt ved hjelp av refleksjon
+    private static void setPrivateField(Object object, String fieldName, Object value) {
+        try {
+            // Henter det private feltet ved hjelp av refleksjon
+            java.lang.reflect.Field field = object.getClass().getDeclaredField(fieldName);
+            // Gjør det private feltet tilgjengelig for endringer
+            field.setAccessible(true);
+            // Setter verdien til det private feltet
+            field.set(object, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail("Feilet å sette mock-objektet: " + e.getMessage());
+        }
     }
 }
